5d91f3eb6e77a956db2457d9bbd42d91ecfd8e67
Klein <arthur.klein@epitech.eu>
2018-09-26 15:56:38 -0400

    fix merge

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -2,42 +2,46 @@
 
 <template>
   <div id="Album" class="container">
-    <div class="box">
       <article class="media">
-        <div class="column is-one-third jaq">
-          <img src="@/assets/album.jpg" alt="Jaquette d'album">
+        <div class="column is-one-third">
+          <img src="@/assets/bigflo-oli-artist.jpg" alt="Jaquette d'album">
         </div>
         <div class="media-content">
           <div class="content">
-            <p class="title is-1">Biglo & Oli - RAP</p>
-
+            <p class="title is-2">Biglo & Oli - RAP</p>
+            <p class="subtitle is-4">2017 - 3 Albums</p>
 
             <div class="extern-buy">
-              <a href="https://itunes.apple.com/fr/artist/bigflo-oli/" target="_blank">
+              <a href="https://itunes.apple.com/fr/artist/bigflo-oli/806433543" target="_blank">
                 <img src="@/assets/itunes_logo.png">
               </a>
             </div>
           </div>
         </div>
       </article>
-    </div>
-    <div class="columns">
-      <div class="column is-2-desktop is-6-mobile">
+    <div class="columns container-list-albums">
+      <div class="column is-2-desktop-only is-6-mobile has-vertically-aligned-content">
+        <a class="play"><i class="far fa-play-circle fa-4x"></i></a>
         <img src="https://bulma.io/images/placeholders/256x256.png">
       </div>
-      <div class="column is-2-desktop is-6-mobile">
+      <div class="column is-2-desktop-only is-6-mobile has-vertically-aligned-content">
+        <a class="play"><i class="far fa-play-circle fa-4x"></i></a>
         <img src="https://bulma.io/images/placeholders/256x256.png">
       </div>
-      <div class="column is-2-desktop is-6-mobile">
+      <div class="column is-2-desktop-only is-6-mobile has-vertically-aligned-content">
+        <a class="play"><i class="far fa-play-circle fa-4x"></i></a>
         <img src="https://bulma.io/images/placeholders/256x256.png">
       </div>
-      <div class="column is-2-desktop is-6-mobile">
+      <div class="column is-2-desktop-only is-6-mobile has-vertically-aligned-content">
+        <a class="play"><i class="far fa-play-circle fa-4x"></i></a>
         <img src="https://bulma.io/images/placeholders/256x256.png">
       </div>
-      <div class="column is-2-desktop is-6-mobile">
+      <div class="column is-2-desktop-only is-6-mobile has-vertically-aligned-content">
+        <a class="play"><i class="far fa-play-circle fa-4x"></i></a>
         <img src="https://bulma.io/images/placeholders/256x256.png">
       </div>
-      <div class="column is-2-desktop is-6-mobile">
+      <div class="column is-2-desktop-only is-6-mobile has-vertically-aligned-content">
+        <a class="play"><i class="far fa-play-circle fa-4x"></i></a>
         <img src="https://bulma.io/images/placeholders/256x256.png">
       </div>
     </div>
@@ -55,6 +59,22 @@
   .extern-buy img {
     max-width: 60px;
   }
+  .container-list-albums div {
+    position:relative
+  }
+  .container-list-albums div .play {
+    position:absolute;
+    color:black;
+    display: none;
+  }
+  .container-list-albums div:hover img {
+    opacity: 0.7;
+  }
+  .container-list-albums div:hover .play{
+    display: block;
+    z-index: 2;
+  }
+
 
   #playlist tr td:nth-child(1),
   #playlist tr td:nth-child(2){
