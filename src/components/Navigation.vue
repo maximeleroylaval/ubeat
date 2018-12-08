f7595beaf3b0fcd55e774c9097586f13d8e3d065
Sébastien Bruere <sebastien.bruere@epitech.eu>
2018-12-08 17:29:32 -0500

    fix burger menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -150,23 +150,22 @@
         }
       }
     },
-  };
+    mounted() {
+      // HANDLE BURGER MENU
+      const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+      if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach((el) => {
+          el.addEventListener('click', () => {
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    if ($navbarBurgers.length > 0) {
-      $navbarBurgers.forEach((el) => {
-        el.addEventListener('click', () => {
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+          });
         });
-      });
+      }
     }
-  });
+  };
 </script>
 
 <style scoped>
